3d211f97360875be9f28d9a8023a4beec3280c99
Victor Rocha de oliveira <roliveira.victor@gmail.com>
2018-11-26 23:25:50 -0200

    modified main activity location

---
+++ app/src/main/java/stonetree/com/mercadolivre/home/view/MainActivity.java
@@ -0,0 +1,15 @@
+package stonetree.com.mercadolivre.home.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import stonetree.com.mercadolivre.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
